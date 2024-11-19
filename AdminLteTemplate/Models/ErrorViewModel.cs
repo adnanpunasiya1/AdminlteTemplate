7058d1e518ef57c0a526a4d935828bd6bd392202
adnanpunasiya1 <135577744+adnanpunasiya1@users.noreply.github.com>
2024-11-19 11:26:26 +0530

    intial commit

---
+++ AdminLteTemplate/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AdminLteTemplate.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
